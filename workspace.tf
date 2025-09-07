b41757aab0759bfc7b86f0edb07471979ac8d206
Leon <leonlaf66@gmail.com>
2025-09-07 12:48:41 -0400

    test

--- workspace.tf
+++ workspace.tf
@@ -51,8 +51,6 @@ resource "databricks_metastore" "this" {
 
 # This resource is only created if a metastore is being created OR an existing one is provided.
 resource "databricks_metastore_assignment" "this" {
-  count = local.assigned_metastore_id != null ? 1 : 0
-
   metastore_id = local.assigned_metastore_id
   workspace_id = databricks_mws_workspaces.this.workspace_id
 }
